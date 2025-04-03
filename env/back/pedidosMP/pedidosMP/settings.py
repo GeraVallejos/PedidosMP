0fe091c42388cd824c2f507748209c5dfd5118ae
GeraVallejos <ger.vallejos@hotmail.com>
2025-04-03 16:36:56 -0300

    Pass arreglados

--- env/back/pedidosMP/pedidosMP/settings.py
+++ env/back/pedidosMP/pedidosMP/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
-from secretos import secret_key
+from secreto import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'aisdb',
         'USER': 'root',
-        'PASSWORD': 'Lorddeoz86.',
+        'PASSWORD': pass_bd,
         'HOST': 'localhost',
         'PORT': '3306',
     }
